2e5dc5950f24493a297b98b649170c36e27f9e80
Hasan Mawassi <hasanmawassi0@gmail.com>
2026-01-20 17:50:12 +0200

    feat: add database migrations for chunk count functionality

---
+++ rag-server/prisma/migrations/20260120132356_simplify_to_chunk_count_only/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the `DocumentChunk` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "DocumentChunk" DROP CONSTRAINT "DocumentChunk_documentId_fkey";
+
+-- AlterTable
+ALTER TABLE "Document" ADD COLUMN     "chunkCount" INTEGER NOT NULL DEFAULT 0;
+
+-- DropTable
+DROP TABLE "DocumentChunk";
